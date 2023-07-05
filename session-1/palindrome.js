bd9a0ef1bc6b5f8a10eb8c7174ee6549752916a8
Angelo Dacasin <angelo.dacasin@gcash.com>
2023-07-05 23:14:17 +0800

    Homework palindrome.js [ABDacasin]
    Homework palindrome.js [ABDacasin]

--- session-1/palindrome.js
+++ session-1/palindrome.js
@@ -6,19 +6,17 @@ A palindrome is a word that, when reversed, is the same word.
 Examples include racecar, deified, civic, radar, uwu, et al.
 */
 
-
 function isPalindrome(word) {
   const lowerCaseReversedWord = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-  const reversedWord = lowerCaseReversedWord.split('').reverse().join('')
-    return lowerCaseReversedWord === reversedWord
-
+  const reversedWord = lowerCaseReversedWord.split('').reverse().join('');
+  return lowerCaseReversedWord === reversedWord;
 }
 
 console.log(isPalindrome("racecar"));  // true
 console.log(isPalindrome("deified"));  // true
 console.log(isPalindrome("civic"));    // true
 console.log(isPalindrome("radar"));    // true
-console.log(isPalindrome("uwu"));    // true
-console.log(isPalindrome("Uwu"));    // true
-console.log(isPalindrome("Taco cat"));    // true
-console.log(isPalindrome("angelo"));    // false
+console.log(isPalindrome("uwu"));      // true
+console.log(isPalindrome("Uwu"));      // true
+console.log(isPalindrome("Taco cat")); // true
+console.log(isPalindrome("angelo"));   // false
