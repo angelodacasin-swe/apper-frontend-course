71042edaead06f1712b8c51bd30cfad3613d9ec0
Angelo Dacasin <angelo.dacasin@gcash.com>
2023-07-05 21:34:33 +0800

    Homework palindrome.js - ABDacasin
    Homework palindrome.js - ABDacasin

--- session-1/palindrome.js
+++ session-1/palindrome.js
@@ -7,3 +7,18 @@ Examples include racecar, deified, civic, radar, uwu, et al.
 */
 
 
+function isPalindrome(word) {
+  const lowerCaseReversedWord = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const reversedWord = lowerCaseReversedWord.split('').reverse().join('')
+    return lowerCaseReversedWord === reversedWord
+
+}
+
+console.log(isPalindrome("racecar"));  // true
+console.log(isPalindrome("deified"));  // true
+console.log(isPalindrome("civic"));    // true
+console.log(isPalindrome("radar"));    // true
+console.log(isPalindrome("uwu"));    // true
+console.log(isPalindrome("Uwu"));    // true
+console.log(isPalindrome("Taco cat"));    // true
+console.log(isPalindrome("angelo"));    // false
