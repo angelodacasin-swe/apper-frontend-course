1ce0c16aad48807d0b9d345f273fa4b78894ab88
Angelo Dacasin <angelo.dacasin@gcash.com>
2023-07-05 23:14:33 +0800

    Homework vowels.js [ABDacasin]
    Homework vowels.js [ABDacasin]

--- session-1/vowels.js
+++ session-1/vowels.js
@@ -9,15 +9,15 @@ console.log(wordWithOnlyVowels)
 
 function convertToVowels(word) {
   const wordWithOnlyVowels = word.match(/[aeiou]/g) // variable 'wordWithOnlyVowels' contains an array containing vowels.
-    .join('') // this will convert the resulting array to a string
+    .join(''); // this will convert the resulting array to a string
   
-  if(wordWithOnlyVowels) {
-      return wordWithOnlyVowels
+  if (wordWithOnlyVowels) {
+    return wordWithOnlyVowels;
   } else {
-      return "There are no vowels found in the string."
+    return "There are no vowels found in the string.";
   }
 }
 
-const word = 'thisissomeword' // variable 'word' contains the string 'thisissomeword'
-const wordWithOnlyVowels = convertToVowels(word) // perform the function on 'convertToVowels' and apply it on the word variable. Assign it then to the 'wordWithOnlyVowels' variable
-console.log(wordWithOnlyVowels) // 'iioeo'
+const word = 'thisissomeword'; // variable 'word' contains the string 'thisissomeword'
+const wordWithOnlyVowels = convertToVowels(word); // perform the function on 'convertToVowels' and apply it on the word variable. Assign it then to the 'wordWithOnlyVowels' variable
+console.log(wordWithOnlyVowels); // 'iioeo'
